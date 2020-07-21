6664283b3db954878df65f20f337c5836eb7053f
nguyendm77 <nguyendm77@gmail.com>
2020-07-20 22:05:13 -0700

    modified code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import IngredientList from './IngredientList';
-import BurgerPane from './BurgerPane';
+import BurgerPane from './BurgerPane'
 import './App.css';
 
-class App extends Component{
-  state = {
-    ingredients: this.props.ingredients,
-    stack: []
-  }
 
-  handleClick = (e, id) => {
+const menuList = 
+    [
+      {name: 'Kaiser Bun', color: 'saddlebrown'},
+      {name: 'Sesame Bun', color: 'sandybrown'},
+      {name: 'Gluten Free Bun', color: 'peru'},
+      {name: 'Lettuce Wrap', color: 'olivedrab'},
+      {name: 'Beef Patty', color: '#3F250B'},
+      {name: 'Soy Patty', color: '#3F250B'},
+      {name: 'Black Bean Patty', color: '#3F250B'},
+      {name: 'Chicken Patty', color: 'burlywood'},
+      {name: 'Lettuce', color: 'lawngreen'},
+      {name: 'Tomato', color: 'tomato'},
+      {name: 'Bacon', color: 'maroon'},
+      {name: 'Onion', color: 'lightyellow'}
+    ]
+
+
+function App(props) {
+
+  const [ingredients, setIngredients] = useState(menuList)
+  const [stack, setStack] = useState([])
+
+  const handleClick=(e, id) => {
     console.log(e.target.name)
-    const newStack = [...this.state.stack]
-    newStack.unshift(this.state.ingredients[id])
-    this.setState({
-      stack: newStack
-    })
+    const newStack = [...stack]
+    newStack.unshift(ingredients[id])
+    setStack(newStack)
   }
 
-  handleClear = (e) => {
+  const handleClear = (e) => {
     console.log("does this work?")
-    this.setState({
-      stack: []
-    })
+    setStack([])
   }
 
-  render(){
-    return(
-      <div className="App">
-        <div class="menu">
-          <IngredientList allIngredients={this.state.ingredients} moveIngredient={this.handleClick} />
-        </div>
-        <div class="stack">
-          <BurgerPane  burger={this.state.stack} clear={this.handleClear} />
-        </div> 
+  
+  return(
+    <div className="App">
+      <div class="menu">
+        <IngredientList allIngredients={ingredients} moveIngredient={handleClick} />
       </div>
-    )
-  }
+      <div class="stack">
+        <BurgerPane  burger={stack} clear={handleClear} />
+      </div> 
+    </div>
+  )
 }
 
 export default App;
